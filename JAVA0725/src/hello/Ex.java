fa537504b409ada61de25baaacd3227c392071b3
ITWILL <ITWILL@DESKTOP-FKD5EKF>
2022-09-05 12:47:36 +0900

    0725_주석/데이터타입/변수/작성규칙

---
+++ JAVA0725/src/hello/Ex.java
@@ -0,0 +1,17 @@
+package hello;
+
+public class Ex {
+
+	public static void main(String[] args) {
+		/*
+		 * 자동 완성(Content Assist) 단축키 : [Ctrl] + [Space bar]
+		 */
+		System.out.println("Hello Java!");
+		System.out.println("김지희");
+		System.out.println("아이티윌 부산교육센터");
+		
+		System.out.println();
+	}
+}
+
+ 
