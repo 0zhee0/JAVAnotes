63cd064b5282d472e8ecbf0f5cfac8e7b68b9a78
0zhee0 <0zhee0@gmail.com>
2022-11-16 11:32:10 +0900

    221116_자바Test

---
+++ JAVA1005/src/polymorphism/aaadrf.java
@@ -0,0 +1,22 @@
+package polymorphism;
+
+public class aaadrf {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			
+		
+		
+		double dNum1 = 1.2;
+		float fNum2=0.9F;
+		
+		int iNum3 = (int)dNum1 + (int)fNum2;
+		int iNum4 = (int)(dNum1 + fNum2);
+		
+		System.out.println(iNum3);
+		System.out.println(iNum4);
+		
+		int num = (int) 40.0;
+	}
+
+}
